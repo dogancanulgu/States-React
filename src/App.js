0bea7c0c9a8d4be3180cac5487a5b7f92fbfe180
Dogancan Ulgu <dogancanulgu@gmail.com>
2021-07-18 22:03:34 +0300

    Object States

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   const [name, setName] = useState("Doğancan");
   const [age, setAge] = useState(27);
   const [friends, setFriends] = useState(["Ahmet", "Murat"]);
+  const [address, setAddress] = useState({ title: "İstanbul", zip: 34188});
 
   return (
     <div className="App">
@@ -21,6 +22,15 @@ function App() {
         <div key={index}>{friend}</div>))
       }
       <button onClick={() => setFriends([...friends, "Ayşe"])}>Add New Friend</button>
+      <hr />
+      <br />
+
+    <h2>Address</h2>
+    <div>{address.title} {address.zip}</div>
+    <br />
+
+    <button onClick={() => setAddress({...address, title:"Ankara", zip:44444})}>Change the Address</button>
+
 
 
     </div>

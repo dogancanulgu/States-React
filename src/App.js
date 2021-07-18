8b4d3dc0892b9e4c8973d00008e7906b2afb8cf4
Dogancan Ulgu <dogancanulgu@gmail.com>
2021-07-18 20:43:47 +0300

    Array States

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {useState} from "react";
 function App() {
   const [name, setName] = useState("Doğancan");
   const [age, setAge] = useState(27);
+  const [friends, setFriends] = useState(["Ahmet", "Murat"]);
 
   return (
     <div className="App">
@@ -12,6 +13,16 @@ function App() {
 
       <button onClick={() => setName("Ahmet")}>Change Name</button>
       <button onClick={() => setAge(25)}>Change Age</button>
+      <hr />
+      <br />
+      <h2>Friends</h2>
+      {
+        friends.map( (friend, index) => (
+        <div key={index}>{friend}</div>))
+      }
+      <button onClick={() => setFriends([...friends, "Ayşe"])}>Add New Friend</button>
+
+
     </div>
   );
 }
